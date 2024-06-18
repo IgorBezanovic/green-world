fa618636251e99fea4810a7bf681ca01c955c43d
i.bezanovic <i.bezanovic@levi9.com>
2024-06-18 16:29:40 +0200

    gw: Fix mulitple canonical links

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,9 +1,13 @@
 import { Search } from '@green-world/components';
 import clsx from 'clsx';
+import { Helmet } from 'react-helmet';
 
 export const HomePage = () => {
   return (
     <div className={clsx('w-full', 'bg-whiteLinen', 'min-h-[100vh]')}>
+      <Helmet>
+        <link rel="canonical" href="https://www.green-world-six.vercel.app/" />
+      </Helmet>
       <div
         className={clsx('max-w-[1400px]', 'mx-auto', 'p-5', 'flex', 'gap-5')}
       >
