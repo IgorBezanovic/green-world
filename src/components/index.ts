5dd0a3f4838e7873ec0f265724e86ba6f4e3596b
IgorBezanovic <igorbezanovic@gmail.com>
2024-06-27 22:12:38 +0200

    feat(#4): Introduced footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from '@green-world/components/BackButton';
 export * from '@green-world/components/Divider';
+export * from '@green-world/components/Footer';
 export * from '@green-world/components/ForgotPasswordForm';
 export * from '@green-world/components/Header';
 export * from '@green-world/components/Layout';
