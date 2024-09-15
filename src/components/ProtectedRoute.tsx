e8ab08be5fcb7baf059656014befdda63df8e1ef
IgorBezanovic <igorbezanovic@gmail.com>
2024-09-16 00:00:08 +0200

    fix: Fixed protected route redirect

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,11 @@
 import { getItem } from '@green-world/utils/cookie';
-import { jwtDecode } from 'jwt-decode';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
 import { ElementType } from 'react';
 import { Navigate } from 'react-router-dom';
+
+interface CustomJwtPayload extends JwtPayload {
+  _id: string;
+}
 
 export const ProtectedRoute = ({
   element: Component,
@@ -9,7 +13,14 @@ export const ProtectedRoute = ({
 }: {
   element: ElementType;
 }) => {
-  const token: any = jwtDecode(getItem('token') || '');
+  const localStorageToken = getItem('token');
+  const token: false | CustomJwtPayload = localStorageToken
+    ? jwtDecode(localStorageToken)
+    : false;
 
-  return token._id ? <Component {...rest} /> : <Navigate to="/login" />;
+  return token && token._id ? (
+    <Component {...rest} />
+  ) : (
+    <Navigate to="/login" />
+  );
 };
