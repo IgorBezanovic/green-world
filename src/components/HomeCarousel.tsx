cedbc2c7da7a937914048e00bd7a837d86c9d60a
i.bezanovic <igor.bezanovic@aws.talpanetwork.com>
2024-11-12 13:34:30 +0100

    feat: Fetch product by gorup on home page

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -1,4 +1,4 @@
-import { Carousel, Empty } from 'antd';
+import { Carousel, Empty, Skeleton } from 'antd';
 import clsx from 'clsx';
 
 import '../styles.css';
@@ -8,32 +8,34 @@ export const HomeCarousel = ({ ...props }) => {
   const products = Array.isArray(props.products) ? props.products : [];
 
   return products && products.length > 0 ? (
-    <Carousel
-      arrows
-      draggable={true}
-      infinite
-      slidesToShow={4}
-      responsive={[
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2
+    <Skeleton loading={props.isLoading} active>
+      <Carousel
+        arrows
+        draggable={true}
+        infinite
+        slidesToShow={4}
+        responsive={[
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2
+            }
           }
-        }
-      ]}
-      slidesToScroll={1}
-      className={clsx('w-full')}
-    >
-      {products.map((product: any) => (
-        <div key={product.title} className="px-1">
-          <ProductCard
-            product={product}
-            loading={props.isLoading}
-            style={'h-100%'}
-          />
-        </div>
-      ))}
-    </Carousel>
+        ]}
+        slidesToScroll={1}
+        className={clsx('w-full')}
+      >
+        {products.map((product: any) => (
+          <div key={product.title} className="px-1">
+            <ProductCard
+              product={product}
+              loading={props.isLoading}
+              style={'h-100%'}
+            />
+          </div>
+        ))}
+      </Carousel>
+    </Skeleton>
   ) : (
     <Empty />
   );
