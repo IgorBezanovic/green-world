10dc1e76579d117b506c2606ff8a2e02ee41ee25
Igor Bežanović <igorbezanovic@gmail.com>
2026-01-04 13:47:06 +0100

    Add breadcrumbs and meta tags to BlogPostPage
    Enhanced the BlogPostPage by introducing AppBreadcrumbs for navigation and MetaTags for SEO. Also refactored layout to use MUI Box for improved styling. Updated tsconfig.json to exclude build and coverage directories from TypeScript compilation.

--- src/views/BlogPostPage/BlogPostPage.tsx
+++ src/views/BlogPostPage/BlogPostPage.tsx
@@ -4,7 +4,9 @@ import {
   VoteButtons,
   CommentForm,
   CommentList,
-  ZSLogoLogoMark
+  ZSLogoLogoMark,
+  AppBreadcrumbs,
+  MetaTags
 } from '@green-world/components';
 import { formatImageUrl } from '@green-world/utils/helpers';
 import { Box, Card, Chip, useTheme } from '@mui/material';
@@ -66,89 +68,122 @@ export const BlogPostPage = () => {
       </div>
     );
 
-  return (
-    <div className="max-w-3xl mx-auto py-8 px-4">
-      {post?.keywords && post.keywords.length > 0 && (
-        <div className="flex flex-wrap gap-3 mb-4">
-          <ZSLogoLogoMark
-            color={theme.palette.secondary.main}
-            width="24px"
-            height="42spx"
-          />
-          {post.keywords.map((kw) => (
-            <Chip key={kw} label={kw} variant="outlined" color="secondary" />
-          ))}
-        </div>
-      )}
-      <h1 className="text-4xl font-bold mb-6">{post?.title}</h1>
+  const pages = [
+    { label: 'Početna', route: '/' },
+    { label: 'Blog', route: '/blog' },
+    { label: post?.title || '', route: `/blog/${post?._id}` }
+  ];
 
-      <div className="flex items-center mb-6 gap-3">
-        <div className="flex items-center gap-1 text-sm text-gray-500">
-          <User />
-          {post?.author}
-        </div>
-        <div className="flex items-center gap-1 text-sm text-gray-500">
-          <Calendar />
-          {new Date(post?.createdAt || '').toLocaleDateString()}
-        </div>
-        {post?.timeOfReading && (
-          <div className="flex items-center gap-1 flex flex-wrap gap-2 text-sm text-gray-500">
-            <Clock />
-            {post.timeOfReading} min čitanja
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        backgroundColor: 'background.paper',
+        minHeight: 'calc(100vh - 360px)'
+      }}
+    >
+      <MetaTags title={'Zeleni svet | Blog'} />
+      <Box
+        sx={(theme) => ({
+          maxWidth: '1400px',
+          width: '100%',
+          mx: 'auto',
+          px: '16px',
+          py: '1.75rem',
+          gap: 4,
+          [theme.breakpoints.up('sm')]: {
+            px: '1.5rem'
+          },
+          [theme.breakpoints.up('xl')]: {
+            px: 0
+          },
+          display: 'flex',
+          flexDirection: 'column'
+        })}
+      >
+        <AppBreadcrumbs pages={pages} />
+        {post?.keywords && post.keywords.length > 0 && (
+          <div className="flex flex-wrap gap-3 mb-4">
+            <ZSLogoLogoMark
+              color={theme.palette.secondary.main}
+              width="24px"
+              height="42spx"
+            />
+            {post.keywords.map((kw) => (
+              <Chip key={kw} label={kw} variant="outlined" color="secondary" />
+            ))}
           </div>
         )}
-      </div>
+        <h1 className="text-4xl font-bold mb-6">{post?.title}</h1>
 
-      {post?.coverImage && (
-        <img
-          src={formatImageUrl(post?.coverImage || '')}
-          alt={post?.title}
-          className="w-full rounded mb-4"
-        />
-      )}
+        <div className="flex items-center mb-6 gap-3">
+          <div className="flex items-center gap-1 text-sm text-gray-500">
+            <User />
+            {post?.author}
+          </div>
+          <div className="flex items-center gap-1 text-sm text-gray-500">
+            <Calendar />
+            {new Date(post?.createdAt || '').toLocaleDateString()}
+          </div>
+          {post?.timeOfReading && (
+            <div className="flex items-center gap-1 flex flex-wrap gap-2 text-sm text-gray-500">
+              <Clock />
+              {post.timeOfReading} min čitanja
+            </div>
+          )}
+        </div>
 
-      <div>
-        {post?.blocks?.map((b) => (
-          <BlogBlock key={b._id} block={b} />
-        ))}
-      </div>
+        {post?.coverImage && (
+          <img
+            src={formatImageUrl(post?.coverImage || '')}
+            alt={post?.title}
+            className="w-full rounded mb-4"
+          />
+        )}
 
-      <Box sx={{ display: 'flex', gap: 4, my: 8, alignItems: 'center' }}>
-        <VoteButtons
-          likes={post?.likes}
-          dislikes={post?.dislikes}
-          onVote={handlePostVote}
-        />
-        <Chip
-          aria-label="share"
-          variant="outlined"
-          color="secondary"
-          onClick={() => {
-            navigator.clipboard
-              .writeText(`https://www.zelenisvet.rs/blog/${post?._id}`)
-              .then(() => {
-                toast.success('Kopiran link');
-              })
-              .catch(() => {
-                alert('Neuspešno kopiranje linka');
-              });
-          }}
-          label={
-            <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-              <Copy /> Podeli Blog
-            </Box>
-          }
-        />
+        <div>
+          {post?.blocks?.map((b) => (
+            <BlogBlock key={b._id} block={b} />
+          ))}
+        </div>
+
+        <Box sx={{ display: 'flex', gap: 4, my: 8, alignItems: 'center' }}>
+          <VoteButtons
+            likes={post?.likes}
+            dislikes={post?.dislikes}
+            onVote={handlePostVote}
+          />
+          <Chip
+            aria-label="share"
+            variant="outlined"
+            color="secondary"
+            onClick={() => {
+              navigator.clipboard
+                .writeText(`https://www.zelenisvet.rs/blog/${post?._id}`)
+                .then(() => {
+                  toast.success('Kopiran link');
+                })
+                .catch(() => {
+                  alert('Neuspešno kopiranje linka');
+                });
+            }}
+            label={
+              <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+                <Copy /> Podeli Blog
+              </Box>
+            }
+          />
+        </Box>
+
+        <Card sx={{ mt: 6, p: 2 }}>
+          <h2 className="text-xl font-semibold mb-3">Ostavite komentar</h2>
+          <CommentForm onSubmit={handleAddComment} />
+          <CommentList
+            comments={post?.comments || []}
+            onReply={handleAddComment}
+          />
+        </Card>
       </Box>
-
-      <Card sx={{ mt: 6, p: 2 }}>
-        <h2 className="text-xl font-semibold mb-3">Ostavite komentar</h2>
-        <CommentForm onSubmit={handleAddComment} />
-        <CommentList
-          comments={post?.comments || []}
-          onReply={handleAddComment}
-        />
-      </Card>
-    </div>
+    </Box>
   );
 };
